7998e12d395153b850eee2f1d741494db0e1278e
MallikSutton <mallik2002.ms@gmail.com>
2024-02-27 17:39:27 -0700

    finished arrow

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow=a, Chars = c) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -29,6 +29,10 @@ int main()
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
       break;
+    case 'a':
+      puts("You selected arrow:");
+      print_arrow(5,9);
+      break;
     case 'q':
       puts("Bye!");
       goto done; 		/* terminate */
@@ -38,7 +42,6 @@ int main()
       printf("Unrecognized option '%c', please try again!\n", c);
     }
   }
-
  done: // To exit from program
   return 0;
 }
